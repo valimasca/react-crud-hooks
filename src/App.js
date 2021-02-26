2ddfd1ed25caa88ee5101ff6f25f8926a812ba14
valimasca <e3skadron@gmail.com>
2021-02-26 15:22:07 -0800

    created userTable and mock data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
 
-function App() {
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'Vali', username: 'valerutz' },
+    { id: 2, name: 'Bethany', username: 'betania' },
+    { id: 3, name: 'Mona', username: 'ramonia' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
